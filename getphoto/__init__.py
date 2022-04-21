4062fa7c7774637a8babe8d270edcd58c0fb32aa
Alexei Ovsiannikov <aovsiannikov@gmail.com>
2022-04-21 22:46:33 +0300

    1. initial commit

---
+++ getphoto/__init__.py
@@ -0,0 +1,6 @@
+__app_name__ = 'getphoto'
+__version__ = '0.1.0'
+
+(FILE_SAVED, NO_PHOTO_ID, NO_ID) = range(3)
+ERRORS = {NO_PHOTO_ID: 'There is no photo for the item with this ID in the technical data table',
+          NO_ID: 'There is no item with this ID in the technical data table'}
